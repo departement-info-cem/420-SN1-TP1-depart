17c35ae561349b5043b51dcd99ea9cd7279a2dad
joris deguet <joris.deguet@gmail.com>
2026-05-22 10:43:04 -0400

    Changement import, retours Mathieu

--- performance.py
+++ performance.py
@@ -1,6 +1,8 @@
-from racine import *
+import racine
 
 # Dans ce fichier vous appellerez les fonctions du module racine pour comparer leurs performances.
-# Vous définirez vous même la fonction en respectant l'énoncé
-# Appellerez votre fonction pour comparer les performances des différentes méthodes de calcul de racine.
-# Au minimum un appel pour comparer 100000 appels de racines entre 1 et float max.
+# - Vous définirez vous même la fonction en respectant l'énoncé
+# - Appellerez votre fonction pour comparer les performances des différentes méthodes de calcul de racine.
+# - Au minimum un appel pour comparer 100000 appels de racines entre 1 et float max.
+#
+# On pourrait par exemple se demander si l'écart de performance est plus petit quand les nombres sont petits.
